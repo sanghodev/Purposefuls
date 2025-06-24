cc36696f79fc0a18c8c8da2a72eef08294043381
sanghodev <80600690+sanghodev@users.noreply.github.com>
2025-06-24 12:48:16 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Purposefuls.github.io/', // GitHub Pages용 base 경로 추가
+  base: '/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
